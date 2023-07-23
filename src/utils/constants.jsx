75c5e3c1cbbab0b60a697af302ed6b3e4ae44db0
Callam Mansell <callammansellau@gmail.com>
2023-07-23 18:49:30 +0100

    dummy data

---
+++ src/utils/constants.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import BuildIcon from '@mui/icons-material/Build';
+import PlumbingIcon from '@mui/icons-material/Plumbing';
+import ElectricityIcon from '@mui/icons-material/Electricity';
+import BrushIcon from '@mui/icons-material/Brush';
+import LandscapeIcon from '@mui/icons-material/Landscape';
+import BricksIcon from '@mui/icons-material/Bricks';
+import RoofingIcon from '@mui/icons-material/Roofing';
+import WeldingIcon from '@mui/icons-material/Welding';
+import FlooringIcon from '@mui/icons-material/Flooring';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+
+const logo = 'https://static.vecteezy.com/system/resources/previews/001/590/595/original/hammer-tool-icon-cartoon-isolated-free-vector.jpg';
+
+const skillsWithIcons = [
+  {
+    id: 1,
+    name: 'Carpentry',
+    icon: <BuildIcon />,
+  },
+  {
+    id: 2,
+    name: 'Plumbing',
+    icon: <PlumbingIcon />,
+  },
+  {
+    id: 3,
+    name: 'Electrical Work',
+    icon: <ElectricityIcon />,
+  },
+  {
+    id: 4,
+    name: 'Painting',
+    icon: <BrushIcon />,
+  },
+  {
+    id: 5,
+    name: 'Landscaping',
+    icon: <LandscapeIcon />,
+  },
+  {
+    id: 6,
+    name: 'Masonry',
+    icon: <BricksIcon />,
+  },
+  {
+    id: 7,
+    name: 'Roofing',
+    icon: <RoofingIcon />,
+  },
+  {
+    id: 8,
+    name: 'Welding',
+    icon: <WeldingIcon />,
+  },
+  {
+    id: 9,
+    name: 'Tiling',
+    icon: <FlooringIcon />,
+  },
+  {
+    id: 10,
+    name: 'HVAC Technician',
+    icon: <AcUnitIcon />,
+  },
+];
+
+export default skillsWithIcons;
